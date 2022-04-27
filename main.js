479da9fd07efc227997d6d87a3452fe3b2775178
Martin Stojmenovski <martinstojmenovskim@gmail.com>
2022-04-26 18:06:04 -0600

    last commit for today

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 // $('input').on('click', function() {
 //     alert("It works")
 // })
-// const hamburger = $(".hamburger").on("click", function(){
-//     classList.toggle("active")
-// })
-// const navMenu = $(".navMenu").on("click", function(){
-//     classList.toggle("active")
-// })
-const hamburger = document.querySelector(".hamburger")
-const navMenu = document.querySelector(".navMenu")
-hamburger.addEventListener("click",()=> {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-})
-document.querySelectorAll(".nav-link").forEach(n =>
-    n.addEventListener("click", () => {
-        hamburger.classList.remove("active")
-        navMenu.classList.remove("active")
-    }))
+
+    $(".hamburger").on("click", function () {
+        $(".navMenu").toggleClass("active")
+        $(".hamburger").toggleClass("active")
+        })
+    $(".nav-link").on("click", function () {
+        $(".navMenu").removeClass("active")
+        $(".hamburger").removeClass("active")
+    })
